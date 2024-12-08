f117c389d5638a932133c2b393e0f7a884516475
ami_AVK <alexkulickov@gmail.com>
2024-12-09 02:29:58 +0300

    image rework

--- dist/chunks/astro_BBi1cU3L.mjs
+++
@@ -1,3 +0,0 @@
-import 'kleur/colors';
-import './astro/server_-1qDrZfG.mjs';
-import 'clsx';
--- dist/chunks/pasport_gPGUa1Zy.mjs
+++
@@ -1,3 +0,0 @@
-const pasport = "/heaton/_astro/pasport.Dh0Acd2c.pdf";
-
-export { pasport as default };
