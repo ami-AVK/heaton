76b79a7a2d6c7c0ca4fb5deaf02a9bdfa0afb7d3
ami_AVK <alexkulickov@gmail.com>
2025-01-17 12:30:49 +0300

    Heaters search works, test for server

--- generateSearchMap.py
+++ generateSearchMap.py
@@ -48,6 +48,59 @@ def create_json_from_directory(root_dir):
                             })
 
     return data
+def create_json_from_directory_powerSearch(root_dir):
+    data = []
+
+    for category in os.listdir(root_dir):
+        if category.startswith('.'):
+            continue
+        category_path = os.path.join(root_dir, category)
+        if os.path.isdir(category_path):
+            for model in os.listdir(category_path):
+                if model.startswith('.'):
+                    continue
+                model_path = os.path.join(category_path, model)
+                if os.path.isdir(model_path):
+                    for csv_file in os.listdir(model_path):
+                        if csv_file.startswith('.') or not csv_file.endswith('.csv'):
+                            continue
+                        csv_path = os.path.join(model_path, csv_file)
+
+                        type_name = os.path.splitext(csv_file)[0]
+                        df = pd.read_csv(csv_path, sep=";")
+                        for index, row in df.iterrows():
+                            item_name = row['Модель']
+
+                            try:
+                                link = os.path.join(category, model, type_name, item_name)
+                            except:
+                                print(row)
+                                continue
+
+                            side = model in ["Compact (C)", "Hygiene Compact (HC)"]
+                            hygiene = model in ["Ventil Compact (VC)", "Hygiene Ventil Compact (HVC)"]
+                            width = int(row['Длина, мм'])
+                            height = int(row['Высота, мм'])
+                            power = int(row['Теплоотдача, Вт'])
+                            panels = int(type_name[0])
+
+                            data.append({
+                                # "Категория": category,
+                                # "Модель": model,
+                                # "Тип": type_name,
+                                "Наименование": item_name,
+                                "Ссылка": link,
+                                "side": side,
+                                "hygiene": hygiene,
+                                "width": width,
+                                "height": height,
+                                "panels": panels,
+                                "power": power
+                            })
+    # Сортируем данные по мощности
+    data.sort(key=lambda x: x['power'])
+    
+    return data
 
 def save_json_to_file(data, output_file):
     with open(output_file, 'w', encoding='utf-8') as file:
@@ -55,8 +108,9 @@ def save_json_to_file(data, output_file):
 
 if __name__ == "__main__":
     root_directory = 'src/data'  # Укажите путь к корневой директории
-    output_file = 'public/search_map.json'  # Укажите имя выходного JSON-файла
+    output_file = 'public/search_map_radiator_power.json'  # Укажите имя выходного JSON-файла
 
     data = create_json_from_directory(root_directory)
+    data = create_json_from_directory_powerSearch(root_directory)
     save_json_to_file(data, output_file)
     print(f"JSON-файл сохранен в {output_file}")
