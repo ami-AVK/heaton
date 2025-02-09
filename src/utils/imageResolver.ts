4e6298d70fe3a2233d8c134cfc9f6a1c9d6b4fe5
ami_AVK <alexkulickov@gmail.com>
2025-02-09 22:25:07 +0300

    Colorize component added

---
+++ src/utils/imageResolver.ts
@@ -0,0 +1,34 @@
+import type { ImageMetadata } from 'astro';
+
+const images = import.meta.glob<{ default: ImageMetadata }>('/src/images/products/*.{jpeg,jpg,png,gif,webp}');
+const FULL_FALLBACK_PATH = "/src/images/products/fullfallback.webp";
+
+function getFallbackImagePath(imagePath: string, model: string): string {
+    const pathParts = imagePath.split('/');
+    pathParts[pathParts.length - 1] = `${model}.webp`;
+    return pathParts.join('/');
+}
+
+export async function resolveImagePath(imagePath: string, model?: string): Promise<ImageMetadata> {
+    const fallbackPath = `/src/images/products/${imagePath}`;
+
+    if (!images[imagePath]) {
+        if (model) {
+            const modelFallbackPath = getFallbackImagePath(imagePath, model);
+            if (images[modelFallbackPath]) {
+                return (await images[modelFallbackPath]()).default;
+            }
+        }
+
+        if (images[fallbackPath]) {
+            return (await images[fallbackPath]()).default;
+        }
+
+        if (!images[FULL_FALLBACK_PATH]) {
+            throw new Error(`${FULL_FALLBACK_PATH} fallback image does not exist`);
+        }
+        return (await images[FULL_FALLBACK_PATH]()).default;
+    }
+
+    return (await images[imagePath]()).default;
+}
